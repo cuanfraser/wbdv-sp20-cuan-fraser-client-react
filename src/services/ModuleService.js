0dc5244f2e5cf50bcd691b50af6acae7e1a9b4c0
Cuan <33520989+cuanfraser@users.noreply.github.com>
2020-02-12 13:33:15 -0500

    Services for Lesson, Module, Topic added

--- src/services/ModuleService.js
+++ src/services/ModuleService.js
@@ -1,17 +1,36 @@
 import { COURSES_URL, MODULES_URL } from "../constants";
 
+export const createModule = (courseId, module) =>
+    fetch(`${COURSES_URL}/${courseId}/modules`, {
+        method: 'POST',
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
 export const findModuleForCourse = (courseId) =>
     fetch(`${COURSES_URL}/${courseId}/modules`)
         .then(response => response.json())
 
+export const findModule = (moduleId) =>
+    fetch(`${MODULES_URL}/${moduleId}`)
+        .then(response => response.json())
+
+
+export const updateModule = (moduleId, module) =>
+    fetch(`${MODULES_URL}/${moduleId}`, {
+        method: 'PUT',
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
 
 export const deleteModule = (moduleId) =>
     fetch(`${MODULES_URL}/${moduleId}`, {
         method: "DELETE"
     })
         .then(response => response.json())
-
-export default {
-    deleteModule,
-    findModuleForCourse
-}
